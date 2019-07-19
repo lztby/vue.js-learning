ca08290f54869dacc53bfd5282abf7bd486b25bd
lizhuo <13037591303@163.com>
2019-07-19 18:01:52 +0800

    修正header

--- my-project/src/pages/detail/conponents/header.vue
+++ my-project/src/pages/detail/conponents/header.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
-    <router-link
-      tag="div"
-      to="/"
-      class="header-abs"
-      v-show="showAbs">
-      <div class="iconfont header-abs-back">&#xe624;</div>
-    </router-link>
-    <div
-      class="header-fixed"
-      v-show="!showAbs"
-      :style="opacityStyle"
-    >
-      <router-link to="/">
-        <div class="iconfont header-fixed-back">&#xe624;</div>
+      <router-link 
+       tag="div"
+       to="/" 
+       class="header-abs" 
+       v-show="showAbs">
+          <div class="iconfont header-abs-back">&#xe60e;</div>
       </router-link>
-      景点详情
+      <div 
+        class="header-fixed"
+        v-show="!showAbs"
+        :style="opacityStyle"
+    >
+        <router-link to= "/">
+            <div class="iconfont header-fixed-back">&#xe60e;</div>
+        </router-link>
+        景点详情
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DetailHeader',
+  name: 'detail_header',
   data () {
     return {
       showAbs: true,
@@ -54,38 +54,38 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/varible.styl'
-  .header-abs
-    position: absolute
-    left: .2rem
-    top: .2rem
-    width: .8rem
-    height: .8rem
-    line-height: .8rem
-    border-radius: .4rem
-    text-align: center
-    background: rgba(0, 0, 0, .8)
-    .header-abs-back
-      color: #fff
-      font-size: .4rem
-  .header-fixed
-    z-index: 2
-    position: fixed
-    top: 0
-    left: 0
-    right: 0
-    height: $headerHeight
-    line-height: $headerHeight
-    text-align: center
-    color: #fff
-    background: $bgcolor
-    font-size: .32rem
-    .header-fixed-back
-      position: absolute
-      top: 0
-      left: 0
-      width: .64rem
-      text-align: center
-      font-size: .4rem
-      color: #fff
+    @import '~styles/varible.styl'
+    .header-abs
+        position: absolute
+        left: .2rem
+        top: .2rem
+        width: .8rem
+        height: .8rem
+        line-height: .8rem
+        border-radius: .4rem
+        text-align: center
+        background: rgba(0, 0, 0, .8)
+        .header-abs-back
+          color: #fff
+          font-size: .4rem
+    .header-fixed
+        z-index: 2
+        position: fixed
+        top: 0
+        left: 0
+        right: 0
+        height: $headerHeight
+        line-height: $headerHeight
+        text-align: center
+        color: #fff
+        background: $bgcolor
+        font-size: .32rem
+        .header-fixed-back
+            position: absolute
+            top: 0
+            left: 0
+            width: .64rem
+            text-align: center
+            font-size: .4rem
+            color: #fff
 </style>
